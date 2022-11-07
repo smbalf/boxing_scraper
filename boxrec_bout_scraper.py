c90ee051c927126533c49d9fd61be23260a82b71
Baudrillard <smbalf+222@gmail.com>
2022-11-07 10:24:31 +0000

    GOT ALL THE DATA EXCEPT ROUND_ENDED

--- boxrec_bout_scraper.py
+++ boxrec_bout_scraper.py
@@ -41,7 +41,7 @@ bout_data_dict = {
             'opp_losses': 'opp_losses',
             'opp_draws': 'opp_draws',
             'bout_result': 'bout_result',
-            'round_ended': 'round_ended'
+            'round_ended': 'round_ended'  # WHY CANT I GET THIS???
         }
     }
 }
@@ -49,21 +49,35 @@ bout_data_dict = {
 def grab_table_data(soup):
     bout_hist_table = soup.find_all('table', {'class': 'dataTable'})
     for table_value in bout_hist_table:
-        date = table_value.find_all('a', href=re.compile('^(/en/date?)'))
-        for dates in date:
-            dates.get_text()
+        date_text = table_value.find_all('a', href=re.compile('^(/en/date?)'))
+        for dates in date_text:
+            date = dates.get_text()
         
         a_tag_text = table_value.find_all('a', href=re.compile('^(/en/box-pro)'))
         for string in a_tag_text:
             grab_url = re.search('href="(.*)">', str(string))
             opp_url = grab_url.group(1)
-            grab_name = re.search('>(.*)</a>', str(string))
-            opp_name = grab_name.group(1)
+            try:
+                grab_name = re.search('>(.*)</a>', str(string))
+                opp_name = grab_name.group(1)
+            except AttributeError:
+                opp_name = string.get_text()
 
-        opp_wins = table_value.find_all('span', {'class': 'textWon'}).get_text()
-        opp_losses = table_value.find_all('span', {'class': 'textLoss'}).get_text()
-        opp_draws = table_value.find_all('span', {'class': 'textDraw'}).get_text()
-        print(f'{opp_wins}/{opp_losses}/{opp_draws}')
+        text_won = table_value.find_all('span', {'class': 'textWon'})
+        for text in text_won:
+            opp_wins = text.get_text()
+        text_lost = table_value.find_all('span', {'class': 'textLost'})
+        for text in text_lost:
+            opp_losses = text.get_text()
+        text_draw = table_value.find_all('span', {'class': 'textDraw'})
+        for text in text_draw:
+            opp_draws = text.get_text()
+
+        text_bout_result = table_value.find_all('div', {'class': 'boutResult bgL'})
+        for text in text_bout_result:
+            bout_result = text.get_text()
+
+
 
 
 launch_soup()
