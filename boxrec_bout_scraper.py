c5c05d066fae69238cc754c078dcc443f53ef180
Baudrillard <smbalf+222@gmail.com>
2022-11-15 14:49:09 +0000

    zzz

--- boxrec_bout_scraper.py
+++ boxrec_bout_scraper.py
@@ -1,6 +1,48 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import pprint
+import csv
+from scrapingbee import ScrapingBeeClient
+
+
+
+def rotate_boxer_urls():
+    # Obtaining API key
+    with open('api_key.txt', 'r') as key_file:
+        API_KEY = key_file.read()
+
+    # Obtaining list of URLs to be scraped
+    with open('boxrec_top_50_urls.csv', 'r') as top_50_urls_csv:
+        reader = csv.reader(top_50_urls_csv)
+        top_50_urls_list = []
+        for row in reader:
+            top_50_urls_list.append(', '.join(row))
+
+    # Creating request to scrape each URL
+    for url in top_50_urls_list:
+        boxrec_url = 'https://boxrec.com' + url
+        api_key = API_KEY
+        PARAMS = {"render_js": 'False'}
+
+        client = ScrapingBeeClient(api_key=api_key)
+
+        # Catching errors and printing
+        try:
+            response = client.get(boxrec_url, params=PARAMS )
+            response.raise_for_status()
+        except Exception as err:
+            print(f'Other error occurred: {err}')
+        else:
+            response.encoding = 'utf-8'
+            # Scrape the page!
+            bs = BeautifulSoup(response.text, 'lxml')
+
+            check_can_scrape(bs)
+            
+            # Below check to see which url was scraped before moving to the next url
+            print(boxrec_url)
+            print('---------------------------------------------')
 
 
 headers = {'User-Agent': 'Mozilla/5.0'}
@@ -28,68 +70,120 @@ def check_can_scrape(soup):
         exit()
     else:
         print(f'SCRAPING...')
-        grab_table_data(soup, boxrec_url)
+        grab_table_data(soup)
 
+def clean_name(name):
+    no_pct = name.replace('%', '')
+    no_slash_n = no_pct.replace('\n', '')
+    no_fwd_slash = no_slash_n.replace('/', '')
+    no_space = no_fwd_slash.replace(' ', '')
+    no_xa0 = no_space.replace('\xa0', '')
+    no_comma = no_xa0.replace(',', ' ')
+    split_name = re.findall('[A-Z][a-z]+', no_comma)
+    cleaned_name = ' '.join(split_name)
+    return cleaned_name
 
-bout_data_dict = {
-    'url': {
-        'bout_date': 'date',
-        'bout_info': {
-            'opp_url': 'opp_url',
-            'opp_name': 'opp_name',
-            'opp_wins': 'opp_wins',
-            'opp_losses': 'opp_losses',
-            'opp_draws': 'opp_draws',
-            'bout_result': 'bout_result',
-            'round_ended': 'round_ended'  # WHY CANT I GET THIS???
-        }
-    }
-}
+boxer_name_list = []
+def grab_table_data(soup):
+    global boxer_name_list
+    boxer_name = clean_name(soup.find('h1').get_text()) # GET BOXER NAME
+    boxer_name_list.append(boxer_name)
 
-def grab_table_data(soup, fighter_url):
-    fighter_url = fighter_url
+    date_list = []
+    opp_url_list = []
+    opp_name_list = []
+    opp_wins_list = []
+    opp_losses_list = []
+    opp_draws_list = []
+    bout_result_list = []
+    
     bout_hist_table = soup.find_all('table', {'class': 'dataTable'})
-
     for table_value in bout_hist_table:
-
         date_text = table_value.find_all('a', href=re.compile('^(/en/date?)'))
         for dates in date_text:
-            date = dates.get_text()
-            
+            date = dates.get_text() # GET ALL BOUT DATES
+            date_list.append(date)
+
         a_tag_text = table_value.find_all('a', href=re.compile('^(/en/box-pro)'))
         for string in a_tag_text:
             grab_url = re.search('href="(.*)">', str(string))
-            opp_url = grab_url.group(1)
+            opp_url = grab_url.group(1) # GET OPPONENT BOXREC URL
+            opp_url_list.append(opp_url)
             try:
                 grab_name = re.search('>(.*)</a>', str(string))
-                opp_name = grab_name.group(1)
+                dirty_opp_name = grab_name.group(1)  # GET OPPONENT NAME
+                opp_name = clean_name(dirty_opp_name)
+                opp_name_list.append(opp_name)
             except AttributeError:
-                opp_name = string.get_text()
+                dirty_opp_name = string.get_text() # GET OPPONENT NAME
+                opp_name = clean_name(dirty_opp_name)
+                opp_name_list.append(opp_name)
 
         text_won = table_value.find_all('span', {'class': 'textWon'})
         for text in text_won:
-            opp_wins = text.get_text()
+            opp_wins = text.get_text() # GET OPPONENT WINS
+            opp_wins_list.append(opp_wins)
 
         text_lost = table_value.find_all('span', {'class': 'textLost'})
         for text in text_lost:
-            opp_losses = text.get_text()
+            opp_losses = text.get_text() # GET OPPONENT LOSSES
+            opp_losses_list.append(opp_losses)
 
         text_draw = table_value.find_all('span', {'class': 'textDraw'})
         for text in text_draw:
-            opp_draws = text.get_text()
+            opp_draws = text.get_text() # GET OPPONENT DRAWS
+            opp_draws_list.append(opp_draws)
 
         text_debut = table_value.select('span[style="font-weight:bold;color:grey;"]')
         for text in text_debut:
-            opp_debut = text.get_text()
-        
+            debut_text = text.get_text()
+            if debut_text == 'debut': # DEBUT BOOLEAN CHECK AND SET
+                opp_debut = True
+            else:
+                opp_debut = False
+
         text_bout_result = table_value.select('div[class*="boutResult"]')
         for text in text_bout_result:
-            bout_result = text.get_text()
+            bout_result = text.get_text() # GET BOUT RESULT
+            bout_result_list.append(bout_result)
+
+    number_of_bouts = list(range(len(date_text), 0, -1)) # GET NUMBER OF BOUTS FOUGHT BY BOXER BEING SCRAPED
+
+    zipped_data = zip(number_of_bouts, date_list, opp_url_list, opp_name_list, opp_wins_list, opp_losses_list, opp_draws_list, bout_result_list)
+    boxrec_dict(boxer_name, zipped_data)
 
 
+# bout_data_dict_list = []
+def boxrec_dict(name, zipped_data):
+    # global bout_data_dict_list
+    bout_data_dict = {
+        name: ({ 
+            bn: {'bout_date': bd, 'opp_url': ou, 'opp_name': on, 'opp_wins': ow, 'opp_losses': ol, 'opp_draws': od, 'bout_result': br} 
+            for bn, bd, ou, on, ow, ol, od, br in zipped_data
+            })
+    }
 
-        
-        
+    #bout_data_dict_list.append(bout_data_dict)
+    #print(bout_data_dict_list)
+
+    #print('writing to csv')
+    #write_to_csv(bout_data_dict_list)
+    create_merged_dict(bout_data_dict, final_dict)
 
 
-launch_soup()
+final_dict = {}
+
+def create_merged_dict(boxer_dict, main_dict):
+    main_dict = {**main_dict, **boxer_dict}
+    
+    
+def write_to_csv(list_of_dict):
+  with open('bout_data.csv','w', encoding="utf-8", newline='') as bout_data_csv:
+    writer = csv.DictWriter(bout_data_csv, fieldnames=boxer_name_list)
+    writer.writeheader()
+    writer.writerows(list_of_dict)
+
+
+# RUNNING THE SCRIPT
+# rotate_boxer_urls() # PROXY
+launch_soup()   # NO PROXY
--- test.py
+++ test.py
@@ -1,20 +1,77 @@
+import pprint
+import csv
+import pandas as pd
+
+dates = ['1-1', '2-1', '3-2', '4-2', '5-3', '6-4', '7-4']
+# number_of_bouts = len(dates)
+boxer_name = 'terry'
+bout_no = ['7', '6', '5', '4', '3', '2', '1']
+opp_name = ['sam', 'jim', 'jack', 'stuart', 'james', 'john', 'simon']
+opp_wins = ['1', '2', '3', '4', '5', '6', '7']
+opp_losses = ['1', '2', '3', '4', '5', '6', '7']
+opp_draws = ['1', '2', '3', '4', '5', '6', '7']
+opp_debut = [False, False, False, False, False, False, False]
+opp_url = ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg']
+bout_result = ['w', 'w', 'w', 'w', 'l', 'w', 'w']
+
+z = zip(bout_no, dates, opp_url, opp_name, opp_wins, opp_losses, opp_draws, opp_debut, bout_result)
+
 bout_data_dict = {
-    'url': {
-        'bout_date': 'date',
-        'bout_info': {
-            'opp_url': 'opp_url',
-            'opp_name': 'opp_name',
-            'opp_wins': 'opp_wins',
-            'opp_losses': 'opp_losses',
-            'opp_draws': 'opp_draws',
-            'bout_result': 'bout_result',
-            'round_ended': 'round_ended'  # WHY CANT I GET THIS???
-        }
+    boxer_name: ({ 
+        bn: {'bout_date': bd, 'opp_url': ou, 'opp_name': on, 'opp_wins': ow, 'opp_losses': ol, 'opp_draws': od, 'opp_debut': odb, 'bout_result': br} 
+        for bn, bd, ou, on, ow, ol, od, odb, br in z
+        })
+    }
+pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(bout_data_dict)
+names = ['terry']
+
+bout_data_dict_list = []
+bout_data_dict_list.append(bout_data_dict)
+
+
+def write_to_csv(list_of_dict):
+    print(list_of_dict)
+    with open('bout_data.csv','w', encoding="utf-8", newline='') as bout_data_csv:
+        writer = csv.DictWriter(bout_data_csv, fieldnames=names)
+        writer.writeheader()
+        writer.writerows(list_of_dict)
+    
+# write_to_csv(bout_data_dict_list)
+
+b = {
+    3: {
+        'ab':3,
+        'ba':9
     }
 }
 
-print(bout_data_dict['url']['bout_info']['bout_result'])
+a = {
+    2: {
+        'ab':1,
+        'ba':2
+    }
+}
 
-bout_data_dict['url']['bout_info']['bout_result'] = 5
+c = {
+    1: {
+        'ab': 4,
+        'ba': 5
+    }
+}
 
-print(bout_data_dict['url']['bout_info']['bout_result'])
+g = {**a, **c}
+print(g)
+
+def create_merged_dict(boxer_dict, main_dict):
+    main_dict = {**main_dict, **boxer_dict}
+    print(main_dict.keys())
+
+    x = pd.DataFrame.from_dict({(i,j): main_dict[i][j] 
+                            for i in main_dict.keys() 
+                            for j in main_dict[i].keys()},
+                        orient='index')
+    
+    print(x)
+
+create_merged_dict(b,g)
